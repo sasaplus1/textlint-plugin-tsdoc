ebd80eb0e18a5e088cc83430745f8672029cbb8e
sasaplus1 <sasaplus1@gmail.com>
2022-12-18 22:30:32 +0900

    add eslint and prettier configs

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+const typescriptExtends = [
+  'eslint:recommended',
+  'plugin:@typescript-eslint/eslint-recommended',
+  'plugin:@typescript-eslint/recommended',
+  'plugin:node/recommended-module',
+  'prettier'
+];
+
+const typescriptPlugins = ['eslint-plugin-tsdoc'];
+
+const tsdocRules = {
+  'tsdoc/syntax': 'warn'
+};
+
+const tryExtensions = ['.d.ts', '.tsx', '.ts', '.jsx', '.js', '.json'];
+
+const config = {};
+const overrides = [];
+
+overrides.push({
+  extends: typescriptExtends,
+  excludedFiles: '*.test.ts',
+  files: ['./**/*.{ts,tsx}'],
+  plugins: [...typescriptPlugins],
+  rules: {
+    ...tsdocRules,
+  },
+  settings: {
+    node: {
+      tryExtensions
+    }
+  }
+});
+
+overrides.push({
+  extends: typescriptExtends,
+  files: ['./**/*.test.ts'],
+  plugins: [...typescriptPlugins],
+  rules: {
+    ...tsdocRules,
+  },
+  settings: {
+    node: {
+      allowModules: ['@textlint/ast-tester'],
+      tryExtensions
+    }
+  }
+});
+
+config.env = {
+  node: true,
+};
+config.extends = ['eslint:recommended', 'plugin:node/recommended', 'prettier'];
+config.overrides = overrides;
+config.parserOptions = {
+  sourceType: 'module'
+};
+config.root = true;
+
+module.exports = config;
