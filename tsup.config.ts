0c398ef85fd9d1ad63e0a558693d0190785f5d28
sasaplus1 <sasaplus1@gmail.com>
2025-07-04 20:08:33 +0900

    update tsconfig.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   dts: true,
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  outDir: 'dist'
+  outDir: 'dist',
   sourcemap: true,
-  target: 'esnext',
+  target: 'esnext'
 });
